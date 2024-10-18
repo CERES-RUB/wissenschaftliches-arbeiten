f787fd57f85ca59aec0afc6e8d9a126b3566c307
Frederik Elwert <frederik.elwert@rub.de>
2024-10-18 20:28:51 +0200

    Format pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,12 @@
 AUTHOR = "Centrum für Religionswissenschaftliche Studien"
 SITENAME = "Leitfaden wissenschaftliches Arbeiten in der Religions­wissenschaft"
 SITEURL = ""
-DESCRIPTION = ("Eine Einführung in Techniken des wissenschaftlichen Arbeitens "
-               "in der Religionswissenschaft. Entstanden am Centrum für "
-               "religionswissenschaftliche Studien (CERES) der Ruhr-Universität "
-               "Bochum.")
+DESCRIPTION = (
+    "Eine Einführung in Techniken des wissenschaftlichen Arbeitens "
+    "in der Religionswissenschaft. Entstanden am Centrum für "
+    "religionswissenschaftliche Studien (CERES) der Ruhr-Universität "
+    "Bochum."
+)
 
 PATH = "content"
 
@@ -28,7 +30,9 @@ TOC_TITLE = "Inhalt"
 # Custom URLs
 # Write all pages simply as their original filename with .html
 FILENAME_METADATA = r"(?:(?P<date>\d{4}-\d{2}-\d{2})_)?(?P<fnslug>.*)"
-ARTICLE_URL = ARTICLE_SAVE_AS = PAGE_URL = PAGE_SAVE_AS = "{fnslug}.html"
+ARTICLE_URL = ARTICLE_SAVE_AS = PAGE_URL = PAGE_SAVE_AS = ARTICLE_LANG_URL = (
+    ARTICLE_LANG_SAVE_AS
+) = PAGE_LANG_URL = PAGE_LANG_SAVE_AS = "{fnslug}.html"
 
 # Menu
 DISPLAY_PAGES_ON_MENU = False
