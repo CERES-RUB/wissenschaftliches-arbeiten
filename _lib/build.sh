2086352f5ced71fd01ce1c2db48b240309edeb12
Frederik Elwert <frederik.elwert@rub.de>
2024-11-19 09:57:43 +0100

    Remove Pflichtliteratur

--- _lib/build.sh
+++ _lib/build.sh
@@ -6,7 +6,7 @@ LIB="$SRC"/../_lib
 DEST="$SRC"/downloads
 FILENAMEBASE=Leitfaden_wissenschaftliches_Arbeiten
 
-DE_FILES=(01_Textarten.md 02_Hausarbeiten.md 03_Referate.md 04_Literatur.md 05_Zitieren.md 06_Textverarbeitung.md 07_Literaturverwaltung.md 08_Präsentationen.md 09_Pflichtliteratur.md 10_Links.md 99_Titel_override.md)
+DE_FILES=(01_Textarten.md 02_Hausarbeiten.md 03_Referate.md 04_Literatur.md 05_Zitieren.md 06_Textverarbeitung.md 07_Literaturverwaltung.md 08_Präsentationen.md 10_Links.md 99_Titel_override.md)
 EN_FILES=(01_Academic_Texts.md 02_Term_Papers.md 03_Presentations.md 04_Literature.md 05_Citation.md 06_Word_Processing.md 07_Reference_Management.md 08_Visual_Presentation.md 99_Title_override.md)
 
 cd "$SRC"/ || exit
